95eac403ae070373a20183474e32625a8d60cc81
Borisov Vlad <borisov.vlad27@gmail.com>
2021-05-04 12:10:27 +0300

    Created project for demo RecyclerView

---
+++ app/src/main/java/com/vladborisov/demorecyclerview/ElementItem.java
@@ -0,0 +1,13 @@
+package com.vladborisov.demorecyclerview;
+
+public class ElementItem {
+    public String title;
+    public String subtitle;
+    public int color;
+
+    public ElementItem(String title, String subtitle, int color) {
+        this.title = title;
+        this.subtitle = subtitle;
+        this.color = color;
+    }
+}
