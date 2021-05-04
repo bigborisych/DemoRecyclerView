bb378301478f1c732cb95a31eab27bd5d10c9663
Borisov Vlad <borisov.vlad27@gmail.com>
2021-05-04 12:34:59 +0300

    In MainActivity.java setup RecyclerView

--- app/src/main/java/com/vladborisov/demorecyclerview/MainActivity.java
+++ app/src/main/java/com/vladborisov/demorecyclerview/MainActivity.java
@@ -1,8 +1,13 @@
 package com.vladborisov.demorecyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = findViewById(R.id.activity_main_recycler);
+        //LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, true); // как вариант
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(new ElementItemsAdapter(LayoutInflater.from(this), new ArrayList<>()));
     }
 }
